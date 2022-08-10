512be6c2123a6244c2b3ceb80650c3f8b2cfd596
Aleksandrs Hoperskis <aleksandrshoperskis@gmail.com>
2022-08-10 18:53:10 +0300

    US2S1T20. US2S1T21, US2S1T22, US2S1T23, US2S1T24

--- src/test/java/pageObjects/Page.java
+++ src/test/java/pageObjects/Page.java
@@ -3,10 +3,18 @@ package pageObjects;
 import flowWorkers.WebDriverLib;
 
 public class Page {
-	
+
 	protected WebDriverLib driver;
+
+
 
 	public Page(WebDriverLib driver) {
 		this.driver = driver;
 	}
+
+
+	public Page() {
+
+	}
+
 }
