e78b873e969aa33d08d2076922453757b92feef4
unknown <tamilla.geydarova@gmail.com>
2022-08-11 17:40:59 +0300

    page fix

--- src/test/java/pageObjects/Page.java
+++ src/test/java/pageObjects/Page.java
@@ -3,12 +3,12 @@ package pageObjects;
 import flowWorkers.WebDriverLib;
 
 public class Page {
-	
-	protected WebDriverLib driver;
 
-	public Page(WebDriverLib driver) {
-		this.driver = driver;
-	}
-	public Page() {
-	}
+    protected WebDriverLib driver;
+
+    public Page(WebDriverLib driver) {
+        this.driver = driver;
+    }
+
 }
+
